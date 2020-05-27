ad27c93eef4c6ec920e29039048a1eecc9f75e42
jyotifeb2 <64646172+jyotifeb2@users.noreply.github.com>
2020-05-28 00:16:19 +0530

    Add files via upload

---
+++ CapitaliseLetter.py
@@ -0,0 +1,12 @@
+# Python program to demonstrate the
+# use of capitalize() function
+
+# capitalize() first letter of
+# string.
+import string
+
+name=input('Enter name : ')
+#name = "jyoti singh"
+capitalized_string = string.capwords(name)
+print(capitalized_string)
+print(name.capitalize())
