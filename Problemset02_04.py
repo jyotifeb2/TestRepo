09779abc4762d223e27c35b252a06a1b317117f8
jyotifeb2 <64646172+jyotifeb2@users.noreply.github.com>
2020-06-06 20:07:19 +0530

    Add files via upload

---
+++ Problemset02_04.py
@@ -0,0 +1,7 @@
+#Write a program that computes the decimal equivalent of the binary number 10011?
+
+binary = input('enter a number: ')
+decimal = 0
+for digit in binary:
+    decimal = decimal*2 + int(digit)
+print (decimal)
